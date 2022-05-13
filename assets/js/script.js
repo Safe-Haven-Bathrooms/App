38da31f61aba45b6457927043bd3d7b6c28420fd
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-12 21:28:32 -0500

    added google map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 //Look a new js file
 
+function initGoogle() {
+    var location = {
+        lat: 37.0902,
+        lng: 95.7129
+    }
+    var options = {
+        center: location, 
+        zoom: 9
+    }
+
+     map = new google.maps.Map(document.getElementById("flush"), options);
+}
+
+
 // API Key Google: AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI
