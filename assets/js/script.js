83ffdbb45254f7038af5166d1d5ec4648ec16c87
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-12 21:49:00 -0500

    refuge api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,11 @@ function initGoogle() {
 }
 
 
+fetch('https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=false&unisex=false&lat=37.7749&lng=122.4194')
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+
+
+
+
 // API Key Google: AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI
