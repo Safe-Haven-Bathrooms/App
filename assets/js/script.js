55d54a562db064c6eb48ab0e9fa722f84eae5b10
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-15 15:29:00 -0500

    lat and lon feature added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,22 @@ var searchEl = $(".container");
 
 function initGoogle() {
     var location = {
-        lat: 37.0902,
-        lng: 95.7129
+        lat: 41.8781136,
+        lng: -87.6297982
     }
     var options = {
         center: location, 
-        zoom: 9
+        zoom: 8
     }
 
      map = new google.maps.Map(document.getElementById("flush"), options);
+
+    //  map marker
+    var marker = new google.maps.Marker({
+        position: {lat:42.0474906,lng:-87.68539299999999},
+        map: map,
+        // icon:
+    });
 }
 
 
@@ -45,6 +52,10 @@ function handleSearchData (event) {
 
     var accessibleCheckbox = $("#Accessible")[0].checked;
     console.log(accessibleCheckbox);
+
+    if (inputField) {
+        getcityCoord(inputField);
+    }
     
     //TODO: Add lat and lon values from Google Maps API
     var refugeRestroomsURL = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=${accessibleCheckbox}&unisex=${unisexCheckbox}&lat=42.045597&lng=-87.688568`
@@ -58,11 +69,38 @@ function handleSearchData (event) {
 searchEl.on('click', '.button', handleSearchData)
 
 
-// fetch('https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada={$}&unisex=false&lat=42.045597&lng=-87.688568')
-//     .then(response=>response.json())
-//     .then(data=>console.log(data))
+function getcityCoord(location) {
+
+    var googleURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location +"&key=AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI"
+   
+
+    fetch(googleURL)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                return response.json()
+            }}) .then(function (data) {
+                console.log(data);
+                var googleLat = data.results[0].geometry.location.lat;
+                console.log(googleLat);
+                var googleLon = data.results[0].geometry.location.lng;
+                console.log(googleLon)
+                var getCoord = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=true&unisex=false&lat=' + googleLat + '&lng=' + googleLon 
+
+                fetch(getCoord).then(function(response) {
+                    console.log(response)
+                    response.json().then(function(data){
+                        console.log(data);
+                    });
+                });
+            });
+};
+        
+
+//  fetch('https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada={$}&unisex=false&lat=42.045597&lng=-87.688568')
+// //     .then(response=>response.json())
+// //     .then(data=>console.log(data))
 
 
 
-
-// API Key Google: AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI
+// let APIKeyGoogle = "AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI"
