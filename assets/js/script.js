78808a65083d8d80917fb4e503f558619dfd6cb2
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-16 10:22:42 -0500

    added geolocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,41 @@ var searchEl = $(".container");
 
 
 function initGoogle() {
-    var location = {
-        lat: 41.8781136,
-        lng: -87.6297982
-    }
-    var options = {
-        center: location, 
-        zoom: 8
-    }
 
-     map = new google.maps.Map(document.getElementById("flush"), options);
+    if (navigator.geolocation) {
+        console.log('geolocation is here!')
 
-    //  map marker
-    var marker = new google.maps.Marker({
-        position: {lat:42.0474906,lng:-87.68539299999999},
-        map: map,
-        // icon:
-    });
+        navigator.geolocation.getCurrentPosition(positionCoord)
+    
+    } else {
+        console.log('geolocation not supported)');
+        map = new google.maps.Map(document.getElementById("flush"), options);
+    }
+};
+
+
+function positionCoord (position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var options = {center: {lat: lat, lng: lng }, zoom: 8}
+    console.log(lat)
+    console.log(lng)
+
+    map = new google.maps.Map(document.getElementById("flush"), options);
+
+//  map marker
+        var marker = new google.maps.Marker({
+            position: { lat: lat, lng: lng},
+            map: map,
+            // icon:
+        });
+
 }
 
 
-function getSearchResults () {
-    
+
+function getSearchResults() {
+
     var ada = true;
     var unisex = true;
     var lat = 42.045597;
@@ -39,7 +52,7 @@ function getSearchResults () {
 getSearchResults();
 
 
-function handleSearchData (event) {
+function handleSearchData(event) {
     event.preventDefault();
     var inputField = $(".input")[0].value;
     console.log(inputField);
@@ -56,50 +69,46 @@ function handleSearchData (event) {
     if (inputField) {
         getcityCoord(inputField);
     }
-    
+
     //TODO: Add lat and lon values from Google Maps API
     var refugeRestroomsURL = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=${accessibleCheckbox}&unisex=${unisexCheckbox}&lat=42.045597&lng=-87.688568`
     console.log(refugeRestroomsURL);
 
     fetch(refugeRestroomsURL)
-        .then(response=>response.json())
-        .then(data=>console.log(data))
-
-}
+        .then(response => response.json())
+        .then(data => console.log(data))
+};
 searchEl.on('click', '.button', handleSearchData)
 
 
 function getcityCoord(location) {
 
-    var googleURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location +"&key=AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI"
-   
+    var googleURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyD4lXBd-dHyZAy38GTGB99wwHqPgpS9JuI"
+
 
     fetch(googleURL)
         .then(function (response) {
             if (response.ok) {
                 console.log(response);
                 return response.json()
-            }}) .then(function (data) {
-                console.log(data);
-                var googleLat = data.results[0].geometry.location.lat;
-                console.log(googleLat);
-                var googleLon = data.results[0].geometry.location.lng;
-                console.log(googleLon)
-                var getCoord = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=true&unisex=false&lat=' + googleLat + '&lng=' + googleLon 
+            }
+        }).then(function (data) {
+            console.log(data);
+            var googleLat = data.results[0].geometry.location.lat;
+            console.log(googleLat);
+            var googleLon = data.results[0].geometry.location.lng;
+            console.log(googleLon)
+            var getCoord = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=true&unisex=false&lat=' + googleLat + '&lng=' + googleLon
 
-                fetch(getCoord).then(function(response) {
-                    console.log(response)
-                    response.json().then(function(data){
-                        console.log(data);
-                    });
+            fetch(getCoord).then(function (response) {
+                console.log(response)
+                response.json().then(function (data) {
+                    console.log(data);
                 });
             });
+        });
 };
-        
 
-//  fetch('https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada={$}&unisex=false&lat=42.045597&lng=-87.688568')
-// //     .then(response=>response.json())
-// //     .then(data=>console.log(data))
 
 
 
