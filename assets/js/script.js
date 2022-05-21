f941008cbcf525a66d537e7e74abab357f15e87c
kkmusau <kkmusau@gmail.com>
2022-05-20 23:13:47 -0500

    removed lettering on the markers in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 var searchEl = $(".container");
 var resultsEl = $("#results");
 var bathroomsArray = [];
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let labelIndex = 0;
+
 
 function initGoogle() {
     var options = {
@@ -136,7 +135,6 @@ function appendLocationDiv(googleLat, googleLon, unisex, accessible) {
                         lat: bathroomInfo.lat,
                         lng: bathroomInfo.lng,
                     },
-                    label: labels[labelIndex++ % labels.length],
                     map: map,
                     title: data[i].name,
                     icon: "./assets/images/tp copy.png"
@@ -232,33 +230,7 @@ function appendLocationDiv(googleLat, googleLon, unisex, accessible) {
                 } else {
                     $(`#list${[i]}`).append(`<li><strong>Comment:</strong> ${bathroomInfo.comment} </li>`);
                 }
-            }
-
-            // for (var i = 0; i < data.length; i++) {
-            //     var lat = JSON.parse(data[i].latitude);
-            //     var lng = JSON.parse(data[i].longitude);
-            //     var name = data[i].name;
-
-            //     var bathroomMarker = new google.maps.Marker({
-            //         position: {
-
-            //             lat: bathroomInfo.lat,
-            //             lng: bathroomInfo.lng,
-            //         },
-            //         // label: labels[labelIndex++ % labels.length],
-            //         map: map,
-            //         title: data[i].name
-            //     });
-
-            //     var infowindow = new google.maps.InfoWindow({
-            //         content: "bathroomInfo"
-            //     });
-
-            //     google.maps.event.addListener(bathroomMarker, 'click', function () {
-            //         infowindow.open(map, bathroomMarker);
-            //     });
-
-            // }
+            } 
         });
     });
 }
