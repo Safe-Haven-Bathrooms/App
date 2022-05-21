11bbf0eb32c201cd0b9f2b530fb0feea81644e6f
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-05-20 21:18:56 -0500

    Added if/else statement to favorites page when there is no saved content

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -5,15 +5,16 @@ function init() {
     var storedBathrooms = JSON.parse(localStorage.getItem("bathrooms"));
     var favoritesEl = $("#favorites");
 
+    if (storedBathrooms !== null) {
     for ( i = 0; i < storedBathrooms.length ; i++){
         console.log("This is the " + [i] + " element");
-
         favoritesEl.append(storedBathrooms[i])
         $(".bathroomDiv").attr("id", "savedBathroom" + [i])
+        }
+        $(".button").attr("hidden", true);
+    } else {
+        favoritesEl.append("You don't have any saved loos!")
     }
-
-    $(".button").attr("hidden", true);
-
-}
+    }
 
 init();
